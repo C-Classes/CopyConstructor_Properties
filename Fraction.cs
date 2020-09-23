097a653eb84b62a9219bc04ea6592b4cc939a75d
PodiWan <38519328+PodiWan@users.noreply.github.com>
2020-09-23 16:40:38 +0300

    Add files via upload

---
+++ Fraction.cs
@@ -0,0 +1,42 @@
+﻿namespace Copy_Props
+{
+    class Fraction
+    {
+        private int _numerator;
+        private int _denominator;
+
+        public int Numerator
+        {
+            get
+            {
+                return _numerator;
+            }
+            set
+            {
+                _numerator = value;
+            }
+        }
+
+        public int Denominator
+        {
+            get => _denominator;
+            set
+            {
+                if (value != 0)
+                    _denominator = value;
+            }
+        }
+
+        public Fraction()
+        {
+            _numerator = 0;
+            _denominator = 1;
+        }
+
+        public Fraction(Fraction copyFraction)
+        {
+            _numerator = copyFraction._numerator;
+            _denominator = copyFraction._denominator;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Copy_Props
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fraction fraction1 = new Fraction();
+            fraction1.Numerator = 10;
+            fraction1.Denominator = 0;
+            
+            Fraction fraction2 = new Fraction(fraction1);
+
+            Console.WriteLine(fraction2.Numerator + "/" + fraction2.Denominator);
+        }
+    }
+}
